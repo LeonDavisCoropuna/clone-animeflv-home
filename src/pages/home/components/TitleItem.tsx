d4dcecc89f234ed39390c19581e2a2c22b4dca6b
leonf <ldavis@unsa.edu.pe>
2023-10-27 00:55:19 -0500

    fix

--- src/pages/home/components/TitleItem.tsx
+++ src/pages/home/components/TitleItem.tsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Emision } from "../models";
 import { BsPlayCircleFill } from "react-icons/bs";
 type Props = {
   item: Emision;
 };
 export const TitleItem: React.FC<Props> = ({ item }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className=" flex items-center py-2 justify-between">
+    <div
+      className=" flex items-center py-2 justify-between"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="flex items-center">
         <div className="pr-2">
-          <BsPlayCircleFill color="#C9C9C9"/>
+          <BsPlayCircleFill color={`${isHovered ? "skyBlue" : "gray"}`} />
         </div>
-        <div className="overflow-hidden text-ellipsis whitespace-nowrap block max-w-[11em] text-[#3b4248]">
+        <div
+          className={`overflow-hidden text-ellipsis whitespace-nowrap block max-w-[11em]  ${
+            isHovered
+              ? "text-sky-400 duration-200"
+              : "text-[#3b4248] duration-200"
+          }`}
+        >
           {item.title}
         </div>
       </div>
 
-      <div className="bg-sky-300 font-bold text-white text-[10px] px-2 rounded-md">
+      <div
+        className={` font-bold text-white text-[10px] px-2 rounded-md ${
+          isHovered ? "bg-sky-500 duration-200" : "bg-sky-300 duration-200"
+        }`}
+      >
         ANIME
       </div>
     </div>
